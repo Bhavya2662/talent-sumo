b2acbf02457ad883577d2108170f6d9c010cf883
Hritik <hritik@Hritiks-MacBook-Air.local>
2022-06-29 11:22:38 +0530

    second commit

--- django_Backend/models.py
+++ django_Backend/models.py
@@ -1,6 +1,20 @@
+from email.headerregistry import ContentDispositionHeader
+from gc import collect
 from msilib.schema import Class
 from django.db import models
 
 # Create your models here.
 
-Class
+# Creating new database Child_scores.
+class chlid_scores_db(models.Model):
+    candidate_id = models.IntegerField(max_length=100)
+    candidate_name = models.CharField(max_length=100)
+    access_id = models.IntegerField(max_length=100)
+    Question_No = models.IntegerField(max_length=100)
+    Likeability = models.IntegerField(max_length=100)
+    Charm = models.CharField(max_length=100)
+    Confidence = models.IntegerField(max_length=100)
+    Fluency = models.IntegerField(max_length=100)
+    Content = models.CharField(max_length=100)
+    Overall = models.IntegerField(max_length=100)
+    
